707a3e393c403083ecd5a27642a79aa4ee655a1f
LeticiaKang <dmswjd_14@naver.com>
2022-09-05 22:28:54 +0900

    팀프로젝트 JAVA 코드 백업 v2

--- src/TermProjectDemo.java
+++ src/TermProjectDemo.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
@@ -37,11 +38,19 @@ public class TermProjectDemo {
                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
                 Statement stmt = conn.createStatement();  //공통으로 사용하기 위해서. 빈화면이 statement고 이걸 class에 사용하고 싶은거임
                 // 문항이 나오는 Query 적기 ** for이용해서 자동으로 넘어갈 수 있도록 수정
-                QUERY = "select Questions from questions " +
-                        "where 1=1 and Questions_ID = 1";
-                stmt.executeUpdate(QUERY);
-                // user에게 답변 받기
-                user_anw = sc.nextLine(); 
+                for (int num = 1; num <5 ; num ++){
+                    QUERY = "select Questions from questions " +
+                            "where 1=1 and Questions_ID = "+num+"";
+                    ResultSet rs = stmt.executeQuery(QUERY);
+                    // Extract data from result set
+                        while (rs.next()){
+                            // Retrieve by column name
+                            System.out.println("Questions: " + rs.getString("Questions"));
+                        }
+                    // user에게 답변 받기
+                    System.out.print("답변을 입력하세요.");
+                    user_anw = sc.nextLine(); 
+                }
     
             } catch (SQLException e) {
                 e.printStackTrace();
