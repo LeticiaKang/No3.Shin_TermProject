f263676b6a70a788e4e6f2fd8f197bd3ec4fb06a
LeticiaKang <dmswjd_14@naver.com>
2022-09-06 11:05:54 +0900

    팀프로젝트 백업V5

--- src/TermProjectDemo.java
+++ src/TermProjectDemo.java
@@ -18,77 +18,119 @@ public class TermProjectDemo {
         String user_name = "0";
         String user_input = "0";
         int user_anw = 1;
+        boolean repeat = true;
         
-        // 요구사항 1. Home화면 (통계 & 설문 중 선택하기)
-        System.out.print("선택하세요(P 또는 S로 입력)>> ");
-    
-        TermProjectPolls polls = new TermProjectPolls();
-        int val = polls.PollFunction();
+        do{
+            // 요구사항 1. Home화면 (통계 & 설문 중 선택하기)
+            System.out.print("선택하세요(P 또는 S로 입력)>> ");
         
-        TermProjectStatics statics = new TermProjectStatics();
-        val = statics.StaticsFunction();
+            TermProjectPolls polls = new TermProjectPolls();
+            int val = polls.PollFunction();
+            
+            TermProjectStatics statics = new TermProjectStatics();
+            val = statics.StaticsFunction();
 
-        user_input = sc.nextLine();
+            user_input = sc.nextLine();
+            
         
-        // class로 만들기 Select
-        if(user_input.compareTo("S") == 0){ 
-            // System.out.println("저는 S에요^^");
-        }
-        else if(user_input.compareTo("P") == 0){ 
-            try {
-                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                Statement stmt = conn.createStatement(); 
-
-                // 1. 사용자 이름을 입력받기
-                System.out.print("이름을 입력하세요>> ");
-                user_name = sc.nextLine();
-                System.out.println("");
-
-                // 2. 입력받은 사용자이름은 client_survey라는 곳에 저장되어야 함.
-                // 2. 해당 컬럽의 max값을 찾아 + 1하여 추가하는 방식
-                max_num_QUERY = "select ifnull(max(Client_ID), 0) as max_col from client_survey";
-                ResultSet rs = stmt.executeQuery(max_num_QUERY);
-                rs.next();
-                int max_mun = rs.getInt("max_col") + 1;
+            if(user_input.compareTo("S") == 0){ 
+                // 1. 통계화면 만들기 (1.이용, 2.시설 및 청결, 3.음료 품질, 4.친절도)
+                // 1. 매우 만족(1) ~ 불만족(5) : 100% ~ 0% 대응 
+                // 1. 1번 질문에 대해 (답 1번(4명), 3번(1명), 5번(2명) → (100*4 + 60*1 + 0*2)/7 = 65.7)
+                // 1. 가로 그래프로 1.이용 0 ■■■■■■■■■■■■■65□□□□□□100 표현? 아님 1.이용 만족도 : 65.7%?
                 
-                QUERY = "insert into client_survey(Client_ID, Name) "  +
-                         "values( "+max_mun+", '"+user_name+"' )";
-                stmt.executeUpdate(QUERY);
+                System.out.println("======이용 만족도 설문 결과 다음과 같습니다.======");
+                
+                try {
+                    Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    Statement stmt = conn.createStatement(); 
+                    
+                    String user_count_QUERY = "select count(*) as user_count from Client_survey"; //설문에 참여한 인원수
+                    ResultSet rs = stmt.executeQuery(user_count_QUERY);
+                    rs.next();
+                    int user_count = rs.getInt("user_count");
+                    // 확인용 
+                    System.out.println("참여자수: " + user_count);
 
-                // 3. 질문이 시작됨. 선택지는 동일. for문으로 반복함
-                // 3. select문을 이용해서 출력
-                for (int num = 1; num <5 ; num ++){
-                    QUERY = "select Questions from questions " +
-                            "where 1=1 and Questions_ID = "+num+"";
-                    rs = stmt.executeQuery(QUERY);
-                    // Extract data from result set
-                        while (rs.next()){
-                            // 3. 질문을 출력함
-                            System.out.println("Questions: " + rs.getString("Questions"));
-                            System.out.println("1.매우 만족  2.만족  3.보통  4.불만족  5.매우불만족");
-                        }
+                    // 문항에 대한 답변의 개수 구해서 평균 구하기
+                    String answer_count_QUERY = "select Questions_ID, Answers_ID,count(Answers_ID) "+
+                                                "from result " +
+                                                "group by Questions_ID, Answers_ID " +
+                                                "order by Questions_ID";
+                    rs = stmt.executeQuery(answer_count_QUERY);
+                    while(rs.next()){
+                        int answer_count = rs.getInt("count(Answers_ID)");
+                        System.out.println("문항번호: "+ rs.getInt( "Questions_ID")  + "답"
+                                            + rs.getInt("Answers_ID") + "개수"
+                                            + rs.getInt("count(Answers_ID)"));
 
-                    // 4. user에게 답변 받아 result table에 저장해야 함.
-                    System.out.print("답변을 입력하세요.");
-                    user_anw = Integer.parseInt(sc.nextLine()); 
+                    }
+                    
+
+
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                
+                
+
+            }
+            else if(user_input.compareTo("P") == 0){ 
+                try {
+                    Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    Statement stmt = conn.createStatement(); 
+
+                    // 1. 사용자 이름을 입력받기
+                    System.out.print("이름을 입력하세요>> ");
+                    user_name = sc.nextLine();
                     System.out.println("");
-                   
-                    QUERY = "insert into result(Questions_ID, Answers_ID, Client_ID) " +
-                         "values( "+num+", "+user_anw+" , "+max_mun+" )";
+
+                    // 2. 입력받은 사용자이름은 client_survey라는 곳에 저장되어야 함.
+                    // 2. 해당 컬럽의 max값을 찾아 + 1하여 추가하는 방식
+                    max_num_QUERY = "select ifnull(max(Client_ID), 0) as max_col from client_survey";
+                    ResultSet rs = stmt.executeQuery(max_num_QUERY);
+                    rs.next();
+                    int max_mun = rs.getInt("max_col") + 1;
+                    
+                    QUERY = "insert into client_survey(Client_ID, Name) "  +
+                            "values( "+max_mun+", '"+user_name+"' )";
                     stmt.executeUpdate(QUERY);
 
-                }
-    
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            
-        }
-        else{
-            System.out.println("다시 입력하세요.");
-            // 나중에 수정하기
-        }
+                    // 3. 질문이 시작됨. 선택지는 동일. for문으로 반복함
+                    // 3. select문을 이용해서 출력
+                    for (int num = 1; num <5 ; num ++){
+                        QUERY = "select Questions from questions " +
+                                "where 1=1 and Questions_ID = "+num+"";
+                        rs = stmt.executeQuery(QUERY);
+                        // Extract data from result set
+                            while (rs.next()){
+                                // 3. 질문을 출력함
+                                System.out.println("Questions: " + rs.getString("Questions"));
+                                System.out.println("1.매우 만족  2.만족  3.보통  4.불만족  5.매우불만족");
+                            }
+
+                        // 4. user에게 답변 받아 result table에 저장해야 함.
+                        System.out.print("답변을 입력하세요.");
+                        user_anw = Integer.parseInt(sc.nextLine()); 
+                        System.out.println("");
+                    
+                        QUERY = "insert into result(Questions_ID, Answers_ID, Client_ID) " +
+                            "values( "+num+", "+user_anw+" , "+max_mun+" )";
+                        stmt.executeUpdate(QUERY);
+
+                    }
         
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+            else{
+                System.out.println("다시 입력하세요.");
+                // 나중에 수정하기
+            }
+        }while(repeat);
         
         return;
     }
